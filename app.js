afc4830cfd2c622ad1391b54a2d3fe685db6a87d
dawnington <dawnington@gmail.com>
2016-07-23 23:20:19 -0700

    added favicon

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
+const favicon = require('serve-favicon');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.set('view engine', 'jade');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use(favicon(__dirname + 'public/images/traveler.png'));
 app.use('/', routes);
 
 app.use(function (req, res, next) {
